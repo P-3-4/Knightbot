12293c28fa24da8c2e42ed673c6e47fbd9f2b8f0
P-3-4 <amirpea4444@gmail.com>
2025-04-06 14:16:00 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -27,7 +27,7 @@ async function pingCommand(sock, chatId) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'KnightBot',
                     serverMessageId: -1
                 }
             }
